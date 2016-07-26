b2911fd8610e3c207a91b843095190338a13d364
Timea Urban <timellya@gmail.com>
2016-07-26 15:17:59 -0700

    Added the full Conditionals homework file

---
+++ conditionals.py
@@ -0,0 +1,69 @@
+# my_name = "Timea"
+# partner_name = "Marla"
+# if my_name > partner_name:
+# 	print "My name is greater!"
+# elif partner_name > my_name:
+# 	print "Their name is greater."
+# else:
+# 	print "Our names are equal!"
+
+# date = 26
+# if date >= 15:
+# 	print "Oh, we're halfway there!"
+# else:
+# 	print "The month is still young."
+
+# today = "Tuesday"
+# if today == "Monday":
+# 	print "Yay, class day!"
+# elif today == "Tuesday":
+# 	print "Sigh, it's only Tuesday."
+# elif today == "Wednesday":
+# 	print "Humpday!"
+# elif today == "Thursday":
+# 	print "#tbt"
+# elif today == "Friday":
+# 	print "TGIF!"
+# else:
+# 	print "Yeah, it's the weekend!"
+
+# age = 17
+
+# # if age >= 18:
+# # 	print "Yay, I can vote!"
+# # else:
+# # 	print "Aww, I cannot vote."
+
+# # if age >= 18 and age >= 21:
+# # 	print "I can vote and go to a bar!"
+
+# if age >= 21:
+# 	print "I can go to a bar."
+# elif age >= 18 and age <=21:
+# 	print "I can vote but cannot go to a bar."
+# else:
+# 	print "I cannot vote or go to a bar."
+
+# number = 9
+# # if number % 2 == 0:
+# # 	print "The number is even."
+# # else:
+# # 	print "The number is odd."
+# if number % 2 != 0:
+# 	print "The number is odd."
+# else:
+# 	print "The number is even."
+
+# if 8 % 2 == 0 and 9 % 2 == 0:
+# 	print "Both numbers are even."
+# else:
+# 	print "Both number are not even"
+
+if 8 % 2 == 8 and 9 % 2 == 0:
+	print "Both numbers are even."
+elif 8 % 2 == 0 and 9 % 2 != 0:
+	print "8 is even and 9 is odd."
+elif 8 % 2 != 0 and 9 % 2 == 0:
+	print "8 is odd and 9 is even."
+else:
+	print "Both numbers are odd."
